86e9e2a7b8e4cd8d57cd4247eb12604eb42af98d
LamoKancho <9014772@student.zadkine.nl>
2022-12-12 11:17:30 +0100

    game

--- Game_Embiya.js
+++ Game_Embiya.js
@@ -5,7 +5,7 @@ const score = document.querySelector('#score')
 
 let result = 0
 let hitPosition
-let currentTime = 60
+let currentTime = 10
 let timerId = null
 
 function randomSquare() {
@@ -23,7 +23,7 @@ squares.forEach(square => {
   square.addEventListener('mousedown', () => {
     if (square.id == hitPosition) {
       result++
-      score.textContent = result
+      // score.textContent = result
       hitPosition = null
     }
   })
@@ -42,7 +42,7 @@ function countDown() {
  if (currentTime == 0) {
    clearInterval(countDownTimerId)
    clearInterval(timerId)
-   alert('GAME OVER! Your final score is ' + result)
+   alert('STAR PLATINA, ZA SCORU DA: ' + result)
  }
 
 }
