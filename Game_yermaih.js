4306a5794e9aa8a87d632c4c478a83db90e34df7
0SamoSenpai0 <9020661@student.zadkine.nl>
2023-01-22 21:25:30 +0100

    lmao game afgemaakt

--- Game_yermaih.js
+++ Game_yermaih.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById('game');
-// const leftPaddle = document.getElementById('player1');
 const context = canvas.getContext('2d');
 const grid = 15;
 const paddleHeight = grid * 5; // 80
 const maxPaddleY = canvas.height - grid - paddleHeight;
+
 function attach_eventhandler(d){
   d.onclick = function(){
       this.style.visibility = "hidden";
@@ -13,14 +13,18 @@ var myArray = document.getElementsByTagName("img");
 for(var d of myArray){
 attach_eventhandler(d);
 }
+// function disableScroll() {
+//   scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+//     }
+function disableScroll() {
+  document.body.classList.add("stop-scrolling");
+}
+
 
 let paddleSpeed = 6;
 let ballSpeed = 5 + 0.1;
-// let puntpunt = ':'
-// let score1 = 0;
-// let score2 = 0;
-// let scoreboard = (score1)-(score2);
-// let beginscreen = "druk op enter om te beginnen";
+
 
 let score_val =
 	document.querySelector('.score_val');
@@ -82,13 +86,12 @@ const ball = {
   // houd in de gaten wanneer de ball gereset moet worden
   resetting: false,
 
-  // ball velocity (start going to the top-right corner)
+  // bal snelheid 
   dx: ballSpeed,
   dy: -ballSpeed
 };
 
-// check for collision between two objects using axis-aligned bounding box (AABB)
-// @see https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+// kijkt of de objecten tegen elkaar gaan
 function collides(obj1, obj2) {
   return obj1.x < obj2.x + obj2.width &&
          obj1.x + obj1.width > obj2.x &&
@@ -102,11 +105,11 @@ function start() {
   context.clearRect(0,0,canvas.width,canvas.height);
 
   
-  // move paddles by their velocity
+  // de peddels laten bewegen
   leftPaddle.y += leftPaddle.dy;
   rightPaddle.y += rightPaddle.dy;
 
-  // prevent paddles from going through walls
+  // zorgt ervoor dat de peddels niet door de muren gaan
   if (leftPaddle.y < grid) {
     leftPaddle.y = grid;
   }
@@ -121,20 +124,17 @@ function start() {
     rightPaddle.y = maxPaddleY;
   }
 
-  // draw paddles
-  // context.fillStyle = '#f4af1b';
+  //tekent de peddels
   context.fillStyle = '#00c0f9';
-  // context.fillStyle = '#1f51ff';
   context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
-  // context.fillStyle = '#fd8579';
   context.fillStyle = '#fd0e0b';
   context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
 
-  // move ball by its velocity
+  // beweegt de bal
   ball.x += ball.dx;
   ball.y += ball.dy;
 
-  // prevent ball from going through walls by changing its velocity
+  // zorgt ervoor dat de bal niet door de muren of peddels gaat
   if (ball.y < grid) {
     ball.y = grid;
     ball.dy *= -1;
@@ -144,18 +144,17 @@ function start() {
     ball.dy *= -1;
   }
 
-  // reset ball if it goes past paddle (but only if we haven't already done so)
+  // reset de bal nadat iemand heeft gescoord
   if ( (ball.x > canvas.width) && !ball.resetting) {
     ball.resetting = true;
     alert("speler 1  heeft gescoord");
     score_val.innerHTML = +score_val.innerHTML + 1;
 
+  // wacht even voordat het de bal terug gooit
     setTimeout(() => {
         ball.resetting = false;
         ball.x = canvas.width / 2;
         ball.y = canvas.height / 2;
-        // ball.x = canvas.width / 5;
-        // ball.y = canvas.height / 5;
       }, 1000);
 
     }   
@@ -165,7 +164,7 @@ function start() {
       alert("speler 2  heeft gescoord");
     score_val2.innerHTML = +score_val2.innerHTML + 1;
 
-    // give some time for the player to recover before launching the ball again
+  // wacht even voordat het de bal terug gooit
     setTimeout(() => {
       ball.resetting = false;
       ball.x = canvas.width / 2;
@@ -174,69 +173,67 @@ function start() {
   
   }
 
-  // check to see if ball collides with paddle. if they do change x velocity
+  // als de bal tegen een peddel gaat, verandert hij zijn richting
   if (collides(ball, leftPaddle)) {
     context.fillStyle = 'blue';  
     context.fillRect(ball.x, ball.y, ball.width, ball.height);
     ball.dx *= -1;
-    ballSpeed + 5.1;
+    ballSpeed + 8;
     
     
 
-    // move ball next to the paddle otherwise the collision will happen again
-    // in the next frame
+    // beweegt de bal een beetje opzij zodat de collision niet 2 keer gebeurd
     ball.x = leftPaddle.x + leftPaddle.width;
   }
   else if (collides(ball, rightPaddle)) {
     ball.dx *= -1;
-    ballSpeed + 5.1;
+    ballSpeed + 8;
 
-    context.fillStyle = 'red';  
-    context.fillRect(ball.x, ball.y, ball.width, ball.height);
-    // move ball next to the paddle otherwise the collision will happen again
-    // in the next frame
+    // context.fillStyle = 'red';  
+    // context.fillRect(ball.x, ball.y, ball.width, ball.height);
+    // beweegt de bal een beetje opzij zodat de collision niet 2 keer gebeurd
     ball.x = rightPaddle.x - ball.width;
   }
 
-  // draw ball
-  // context.fillStyle = '#a3905a';
+  // tekent bal
   context.fillStyle = '#bb13fe';
   context.fillRect(ball.x, ball.y, ball.width, ball.height);
 
-  // draw walls
+  // tekent de muren
   context.fillStyle = 'white';
   context.fillRect(0, 0, canvas.width, grid);
   context.fillRect(0, canvas.height - grid, canvas.width, canvas.height);
 
-  // draw dotted line down the middle
+  // de gestipelde lijn in het midden
   for (let i = grid; i < canvas.height - grid; i += grid * 2) {
     context.fillRect(canvas.width / 2 - grid / 2, i, grid, grid);
   }
 }
 
-// listen to keyboard events to move the paddles
+// luistert naar de keyboard events om de peddels te bewegen
 document.addEventListener('keydown', function(e) {
 
-  // up arrow key
+  // pijltoets omhoog
   if (e.which === 38) {
     rightPaddle.dy = -paddleSpeed;
   }
-  // down arrow key
+  // pijltoets omlaag
   else if (e.which === 40) {
     rightPaddle.dy = paddleSpeed;
   }
 
-  // w key
+  // W toets
   if (e.which === 87) {
     leftPaddle.dy = -paddleSpeed;
   }
-  // a key
+  // S toets
   else if (e.which === 83) {
     leftPaddle.dy = paddleSpeed;
   }
 });
 
-// listen to keyboard events to stop the paddle if key is released
+
+// luistert naar de keyboard events om de peddels te stoppen wanneer je niet meer drukt
 document.addEventListener('keyup', function(e) {
   if (e.which === 38 || e.which === 40) {
     rightPaddle.dy = 0;
@@ -246,53 +243,39 @@ document.addEventListener('keyup', function(e) {
     leftPaddle.dy = 0;
   }
 });
+function disableScroll() {
+  // Get the current page scroll position
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+      // if any scroll is attempted, set this to the previous value
+      window.onscroll = function() {
+          window.scrollTo(scrollLeft, scrollTop);
+      };
+    }
 
 // begin het spel
 document.addEventListener("keydown", function(e){
     if (e.which === 13){
         requestAnimationFrame(start);
-        message.innerHTML = '';
-        message1.innerHTML = '';
-        message2.innerHTML = '';
-        message3.innerHTML = '';
-        // message4.innerHTML = 'druk op "t" voor de menu.';
-        balgif.innerText = '';
-        h1gif.innerText = '';
-        gif.innerText = '';
-        
-        menu.innerHTML = '';
+        message.innerHTML = null;
+        message1.innerHTML = null;
+        message2.innerHTML = null;
+        message3.innerHTML = null;
+        // balgif.innerText = '';
+        // h1gif.innerText = '';
+        // gif.innerText = '';
+        // menu.innerHTML = '';
         score_title.innerHTML = score_val.innerHTML = 0, score_val2.innerHTML = 0;
-        // playerl.innerHTML = 'p1';
-        // playerr.innerHTML = 'p2';
         score_val.innerHTML = '0';
         score_val2.innerHTML = '0';
-        // canvas1.innerHTML = '<canvas width="1000" height="700" id="game"></canvas>';
-        // canvas1.innerHTML = '.canvas{ height: 100vh; width: 100vw;';
-        // gameding.innerHTML = '.canvas{margin-top: 50vh; height: 50vh; padding-right: 50vw;}';
-        function hideimg(){
-          document.getElementById(imageId).style.display = "none";
-        }
-        function gifaway(){
-          hideimg('balpicmain');
-        }
-
     }
-})
-document.addEventListener("keydown", function(e){
-  if (e.which === 13){
-      
-  }
 })
 // document.addEventListener("keydown", function(e){
 //   if (e.which === 13){
 //     document.body.style.background = "url('img/tengen_fight.gif') no-repeat";   
 //     document.body.style.backgroundSize = "cover";
 //   }})
-
-  // document.addEventListener("keydown", function(e){
-  //   if (e.which === 20){
-  //    $('.header').collapse('show');}; });
-
 document.addEventListener("keydown", function(event) {
     console.log(event.which);
   })
